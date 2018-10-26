ea08f38155b706fb1973180d62df021d280574b5
1019157263 <1019157263@qq.com>
2018-10-26 09:18:42 +0800

    用户类的rest——OK

--- qian/urls.py
+++ qian/urls.py
@@ -11,15 +11,20 @@ from django.urls import include
 
 app_name = 'qian'
 
-user_list=views.UserViewSet.as_view({'get':'list','post':'create'})
-user_detail=views.UserViewSet.as_view({'get':'retrieve'})
+router=DefaultRouter()
+router.register(r'UserViewSet',views.UserViewSet)
+
+
 
 
 urlpatterns=[
+url('^api/',include(router.urls)),
 path('',views.index,name='index'),
 path('login/',views.login,name='login'),
 path('sign/',views.sign,name='sign'),
-url('user/', user_list),
-url(r'user_detail/id/(?P<pk>)*', user_detail),
-#url('user_detail/id/<str:pk>', user_detail),
+
+
+# url('user/', user_list),
+# url(r'user_detail/id/(?P<pk>)*', user_detail),
+# #url('user_detail/id/<str:pk>', user_detail),
 ]
